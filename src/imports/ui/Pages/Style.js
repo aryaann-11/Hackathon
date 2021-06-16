72971a33790e09464b445d79869cdc3347e8c4b9
Aashna <aashnakhater@gmail.com>
2021-06-16 15:18:44 +0530

    Basic homepage, will add content cards after this.

---
+++ src/imports/ui/Pages/Style.js
@@ -0,0 +1,13 @@
+import {makeStyles} from '@material-ui/core/styles';
+import { blue, green, grey } from '@material-ui/core/colors';
+
+
+const useStyles = makeStyles((theme)=>({
+
+  container: {
+    paddingTop: '100px',
+  
+  },
+}));
+
+export default useStyles;
