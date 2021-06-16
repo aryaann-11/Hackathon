479e2321b65088fbd3d9c1d57399938f6969a320
aryaann-11 <aryansprivatemail@gmail.com>
2021-06-16 11:29:59 +0530

    auth seems to be working

--- src/imports/ui/App.jsx
+++ src/imports/ui/App.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-
-
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import RootPage from "./Pages/Root";
 export const App = () => (
   <div>
-    
+      <Switch>
+        <Route path="/" exact>
+          <RootPage/>
+        </Route>
+      </Switch>
   </div>
 );
--- src/imports/ui/Header/Navbar.jsx
+++ src/imports/ui/Header/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            Treefer
           </a>
           <button
             className="navbar-toggler"
