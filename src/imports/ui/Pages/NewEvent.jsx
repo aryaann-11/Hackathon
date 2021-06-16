a781864aa98aa5b059e63c5482b10632da601fba
aryaann-11 <aryansprivatemail@gmail.com>
2021-06-16 13:59:32 +0530

    able to load address on clicking map

--- src/imports/ui/Pages/NewEvent.jsx
+++ src/imports/ui/Pages/NewEvent.jsx
@@ -11,6 +11,17 @@ import {
   Map,
 } from "react-leaflet";
 import { useTracker } from "meteor/react-meteor-data";
+import axios from "axios";
+import Navbar from "../Header/Navbar";
+import * as L from "leaflet";
+
+const LeafIcon = L.Icon.extend({
+  options: {},
+});
+const greenIcon = new LeafIcon({
+  iconUrl:
+    "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF",
+});
 
 function LocationMarker() {
   const map = useMapEvents({
@@ -18,6 +29,21 @@ function LocationMarker() {
       console.log(e.latlng);
       document.getElementById("latitude").value = e.latlng.lat;
       document.getElementById("longitude").value = e.latlng.lng;
+      axios
+        .get(
+          "https://nominatim.openstreetmap.org/reverse?lat=" +
+            e.latlng.lat +
+            "&lon=" +
+            e.latlng.lng +
+            "&format=json"
+        )
+        .then((resp) => {
+          console.log(resp.data.display_name);
+          document.getElementById("address").value = resp.data.display_name;
+        })
+        .catch((err) => {
+          alert(err);
+        });
     },
   });
   return <></>;
@@ -28,11 +54,17 @@ const NewEventPage = () => {
   const events = useTracker(() => EventsCollection.find({}));
   const submitNewEvent = (event) => {
     event.preventDefault();
-    lat = parseFloat(event.target.latitude.value);
-    lng = parseFloat(event.target.longitude.value);
-    event_name = event.target.event_name.value;
-    position = [lat, lng];
-    let newEvent = { position: position, name: event_name, host: user.email };
+    const lat = parseFloat(event.target.latitude.value);
+    const lng = parseFloat(event.target.longitude.value);
+    const event_name = event.target.event_name.value;
+    const position = [lat, lng];
+    const address = event.target.address.value;
+    const newEvent = {
+      position: position,
+      name: event_name,
+      host: user.email,
+      address: address,
+    };
     console.log(newEvent);
     EventsCollection.insert(newEvent);
   };
@@ -43,11 +75,13 @@ const NewEventPage = () => {
 
   return (
     <>
+    <Navbar/>
       <div>
         <MapContainer
           center={[51.505, -0.09]}
           zoom={15}
           scrollWheelZoom={false}
+          className="lg-map"
         >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -59,6 +93,7 @@ const NewEventPage = () => {
               <Marker
                 position={[event.position[0], event.position[1]]}
                 key={event._id}
+                icon={greenIcon}
               >
                 <Popup>{event.name}</Popup>
               </Marker>
@@ -71,16 +106,16 @@ const NewEventPage = () => {
         <h4>(click on the map to load latitude and longitude)</h4>
         <form onSubmit={submitNewEvent}>
           <div className="mb-3">
-            <label htmlFor="latitude" className="form-label">
-              Latitude
-            </label>
-            <input type="text" className="form-control" id="latitude" />
+            <input type="hidden" className="form-control" id="latitude" />
           </div>
           <div className="mb-3">
-            <label htmlFor="longitude" className="form-label">
-              Longitude
+            <input type="hidden" className="form-control" id="longitude" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">
+              Address
             </label>
-            <input type="text" className="form-control" id="longitude" />
+            <textarea className="form-control" id="address" />
           </div>
           <div className="mb-3">
             <label htmlFor="event_name" className="form-label">
